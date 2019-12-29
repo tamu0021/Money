35953798a8a37d1324f52cab95c3273723e49bc8
Tamu <iambeat8@yahoo.co,jp>
2019-12-29 11:19:57 +0900

    first commit

---
+++ CalculateIncomeTax/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CalculateIncomeTax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "所得税を計算します。\r\nあなたの収入と控除額を入力してください。";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "収入";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "控除額";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "あなたの所得税は…";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(144, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "です！！";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label7.Location = new System.Drawing.Point(83, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 273);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CalculateIncomeTax/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateIncomeTax
+{
+    public partial class Form1 : Form
+    {
+        private const int NECESSARY_EXPENSE = 380000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int income = 0;
+            int deduction = 0;
+            int taxableIncome = 0;
+            double taxRate = 0.0;
+            int taxDeduction = 0;
+            double taxIncome = 0.0;
+
+            if (CheckTextBox(ref income, ref deduction) == false)
+            {
+                return;
+            }
+
+            taxableIncome = CalculateTaxableIncome(income, deduction);
+
+            CalculateTaxRateAndTaxDeduction(income, ref taxRate, ref taxDeduction);
+
+            taxIncome = CalculateTaxIncome(taxableIncome, taxRate, taxDeduction);
+
+            label7.Text = taxIncome.ToString();
+        }
+
+        #region 内部関数
+
+        private bool CheckTextBox(ref int income, ref int deduction)
+        {
+            if (int.TryParse(GetIncome, out income) == false
+                || int.TryParse(GetDeduction, out deduction) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private int CalculateTaxableIncome(int income, int deduction)
+        {
+            return income - deduction;
+        }
+
+        private void CalculateTaxRateAndTaxDeduction(int income, ref double taxRate, ref int taxDeduction)
+        {
+            if (income <= 1950000)
+            {
+                taxRate = 0.05;
+                taxDeduction = 0;
+            }
+            else if (income <= 3300000)
+            {
+                taxRate = 0.1;
+                taxDeduction = 97500;
+            }
+            else if (income <= 6950000)
+            {
+                taxRate = 0.2;
+                taxDeduction = 427500;
+            }
+            else if (income <= 9000000)
+            {
+                taxRate = 0.23;
+                taxDeduction = 636000;
+            }
+            else if (income <= 18000000)
+            {
+                taxRate = 0.33;
+                taxDeduction = 1536000;
+            }
+            else if (income <= 40000000)
+            {
+                taxRate = 0.4;
+                taxDeduction = 2796000;
+            }
+            else
+            {
+                taxRate = 0.45;
+                taxDeduction = 4796000;
+            }
+        }
+
+        private double CalculateTaxIncome(int taxableIncome, double taxRate, int taxDeduction)
+        {
+            return taxableIncome * taxRate - taxDeduction;
+        }
+
+        #endregion
+
+        #region プロパティ
+
+        /// <summary>
+        /// テキストボックスに入力された収入を値に代入するプロパティ
+        /// </summary>
+        public string GetIncome
+        {
+            get { return textBox1.Text; }
+        }
+
+        /// <summary>
+        /// テキストボックスに入力された控除額を値に代入するプロパティ
+        /// </summary>
+        public string GetDeduction
+        {
+            get { return textBox2.Text; }
+        }
+
+        #endregion
+    }
+}
